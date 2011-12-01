51a151ef3718d3da53d384ec2f3151db5f3629f8
Steve England <steve@wearebeef.co.uk>
2011-12-01 12:36:20 +0000

    Remove cache from articles categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,5 @@
 module Admin
   class CategoriesController < ApplicationController
-    caches_action :index, :expires_in => 1.hour
     def index
       render :json => Article.all_categories
     end
